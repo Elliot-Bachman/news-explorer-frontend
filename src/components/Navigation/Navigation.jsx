c117a8ae973c7213f38466ace83d2931100f1da5
ElliotBachman <elliotbachmanx@gmail.com>
2025-03-25 18:43:46 -0500

    search form button fixes

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -48,20 +48,12 @@ function Navigation({ onLoginClick, onLogout, onRegisterClick, currentPath }) {
             Sign out
           </button>
         ) : (
-          <>
-            <button
-              className="navigation__button navigation__button_type_login"
-              onClick={onLoginClick}
-            >
-              Sign in
-            </button>
-            <button
-              className="navigation__button navigation__button_type_register"
-              onClick={onRegisterClick}
-            >
-              Sign up
-            </button>
-          </>
+          <button
+            className="navigation__button navigation__button_type_login"
+            onClick={onLoginClick}
+          >
+            Sign in
+          </button>
         )}
       </div>
     </nav>
