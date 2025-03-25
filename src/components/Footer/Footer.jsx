f473d26f037a052098ba11fdf1557175173d6623
ElliotBachman <elliotbachmanx@gmail.com>
2025-03-25 17:15:01 -0500

    footer update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,73 +1,52 @@
-import React from "react";
 import "./Footer.css";
+import githubLogo from "/assets/images/github.svg";
+import facebookLogo from "/assets/images/FaceBookIcon.svg";
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__content">
-        <div className="footer__section">
-          <h3 className="footer__title">NewsExplorer</h3>
-          <p className="footer__description">
-            Stay informed with the latest news from around the world.
-          </p>
-        </div>
+      <p className="footer__text">© Supersite, Powered by News API</p>
+      <div className="footer__social">
+        <nav className="footer__nav">
+          <a href="/" className="footer__link">
+            Home
+          </a>
 
-        <div className="footer__section">
-          <h3 className="footer__title">Navigation</h3>
-          <ul className="footer__links">
-            <li>
-              <a href="/" className="footer__link">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="/saved" className="footer__link">
-                Saved Articles
-              </a>
-            </li>
-            <li>
-              <a href="/about" className="footer__link">
-                About
-              </a>
-            </li>
-          </ul>
-        </div>
+          <a
+            href="https://tripleten.com/"
+            className="footer__link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            TriplenTen
+          </a>
+        </nav>
 
-        <div className="footer__section">
-          <h3 className="footer__title">Connect</h3>
-          <div className="footer__social">
-            <a
-              href="https://github.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="footer__social-link"
-            >
-              GitHub
-            </a>
-            <a
-              href="https://linkedin.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="footer__social-link"
-            >
-              LinkedIn
-            </a>
-            <a
-              href="https://twitter.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="footer__social-link"
-            >
-              Twitter
-            </a>
-          </div>
-        </div>
-      </div>
+        <div className="footer__media">
+          <a
+            href="https://github.com/Elliot-Bachman/news-explorer-frontend"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={githubLogo}
+              alt="GitHub Logo"
+              className="footer__github-icon"
+            />
+          </a>
 
-      <div className="footer__bottom">
-        <p className="footer__copyright">
-          © {new Date().getFullYear()} NewsExplorer. All rights reserved.
-        </p>
+          <a
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={facebookLogo}
+              alt="Facebook Logo"
+              className="footer__fb-icon"
+            />
+          </a>
+        </div>
       </div>
     </footer>
   );
