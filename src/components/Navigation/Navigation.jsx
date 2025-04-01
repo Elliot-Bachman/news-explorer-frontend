448d9279b567c1b8dbe97e5df4d3443d93e6826e
ElliotBachman <elliotbachmanx@gmail.com>
2025-04-01 17:02:30 -0500

    Saved article route fix

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -11,45 +11,52 @@ function Navigation({
 }) {
   return (
     <nav className="navigation">
-      <Link to="/" className="navigation__logo">
-        NewsExplorer
-      </Link>
+      <div className="navigation__left">
+        <Link to="/" className="navigation__logo">
+          NewsExplorer
+        </Link>
 
-      <div className="navigation__links">
-        <Link
-          to="/"
-          className={`navigation__link ${
-            currentPath === "/" ? "navigation__link_active" : ""
-          }`}
-        >
-          Home
-        </Link>
-        <Link
-          to="/saved-news"
-          className={`navigation__link ${
-            currentPath === "/saved-news" ? "navigation__link_active" : ""
-          }`}
-        >
-          Saved Articles
-        </Link>
+        <div className="navigation__links">
+          <Link
+            to="/"
+            className={`navigation__link ${
+              currentPath === "/" ? "navigation__link_active" : ""
+            }`}
+          >
+            Home
+          </Link>
+        </div>
       </div>
 
-      <div className="navigation__buttons">
-        {isLoggedIn ? (
-          <button
-            className="navigation__button navigation__button_type_logout"
-            onClick={onLogout}
+      <div className="navigation__right">
+        {isLoggedIn && (
+          <Link
+            to="/saved-news"
+            className={`navigation__link navigation__link--saved ${
+              currentPath === "/saved-news" ? "navigation__link_active" : ""
+            }`}
           >
-            Sign out
-          </button>
-        ) : (
-          <button
-            className="navigation__button navigation__button_type_login"
-            onClick={onLoginClick}
-          >
-            Sign in
-          </button>
+            Saved Articles
+          </Link>
         )}
+
+        <div className="navigation__buttons">
+          {isLoggedIn ? (
+            <button
+              className="navigation__button navigation__button_type_logout"
+              onClick={onLogout}
+            >
+              Sign out
+            </button>
+          ) : (
+            <button
+              className="navigation__button navigation__button_type_login"
+              onClick={onLoginClick}
+            >
+              Sign in
+            </button>
+          )}
+        </div>
       </div>
     </nav>
   );
