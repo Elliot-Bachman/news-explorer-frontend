a180904458d97a448e33b1c3eaeb9d39cd433a88
ElliotBachman <elliotbachmanx@gmail.com>
2025-04-11 02:55:33 -0500

    news card style fixes part 1

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,19 +10,21 @@ function About() {
           className="about__image"
         />
       </div>
-      <div className="about__info">
-        <h3 className="about__title">About the author</h3>
-        <p className="about__paragraph">
-          My name is Elliot Bachman. I am an upcoming full stack software
-          engineer proficient in JavaScript, React, Express, Node, HTML, CSS. I
-          am currently enrolled in the Full Stack Developer program at
-          TripleTen. I look forward to learning more about software development
-          in the future!
-        </p>
-        <p className="about__paragraph">
-          I look forward to learning more about software development in the
-          future!
-        </p>
+      <div className="about__text-wrapper">
+        <div className="about__info">
+          <h3 className="about__title">About the author</h3>
+          <p className="about__paragraph">
+            My name is Elliot Bachman. I am an upcoming full stack software
+            engineer proficient in JavaScript, React, Express, Node, HTML, CSS.
+            I am currently enrolled in the Full Stack Developer program at
+            TripleTen. I look forward to learning more about software
+            development in the future!
+          </p>
+          <p className="about__paragraph">
+            I look forward to learning more about software development in the
+            future!
+          </p>
+        </div>
       </div>
     </section>
   );
